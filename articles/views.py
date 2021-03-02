053467792b98abf3606c96f5a3c2594250d37c2e
wesolyigor <wesolowski.igor@gmail.com>
2021-03-02 16:09:25 +0100

    add pagination

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -6,7 +7,15 @@ from articles.models import Article
 
 def articles_list(request):
     articles = Article.published.all()
-    return render(request, 'articles/list_articles.html', {'articles': articles})
+    paginator = Paginator(articles, 3)
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    return render(request, 'articles/list_articles.html', {'page': page, 'articles': articles})
 
 
 def articles_detail(request, year, month, day, article):
